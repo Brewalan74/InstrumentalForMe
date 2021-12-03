bb63d805750ac252f9ca21e0f65f06a311f21465
Brewalan74 <skeravelorigin@gmail.com>
2021-12-03 12:32:39 +0100

    Feat set up functions.php

--- public/content/themes/instrumentalforme/index.php
+++ public/content/themes/instrumentalforme/index.php
@@ -8,16 +8,11 @@
     <meta name="author" content="" />
     <title>One Page Wonder - Start Bootstrap Template</title>
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-    <!-- Font Awesome icons (free version)-->
-    <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
-    <!-- Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="css/styles.css" rel="stylesheet" />
+
 </head>
 
 <body id="page-top">
+    <?php get_header(); ?>
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
         <div class="container px-5">
@@ -32,7 +27,7 @@
         </div>
     </nav>
     <!-- Header-->
-    <?php get_header(); ?>
+
     <header class="masthead text-center text-white">
         <div class="masthead-content">
             <div class="container px-5">
