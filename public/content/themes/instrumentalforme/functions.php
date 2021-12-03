bde617e1b09ed15fbe8c40b7325e5007ea1a0730
Brewalan74 <skeravelorigin@gmail.com>
2021-12-03 13:23:24 +0100

    Feat update functions.php

--- public/content/themes/instrumentalforme/functions.php
+++ public/content/themes/instrumentalforme/functions.php
@@ -4,19 +4,19 @@
 // echo __FILE__ . ':' . __LINE__;
 // exit();
 
-if (!defined('THEME_GRAYSCALE_VERSION')) {
-    define('THEME_GRAYSCALE_VERSION', '1.0.0');
+if (!defined('THEME_INSTRUMENTALFORME_VERSION')) {
+    define('THEME_INSTRUMENTALFORME_VERSION', '1.0.0');
 }
 
 // Configure theme :
 add_action(
     'after_setup_theme',
-    'grayscale_initializeTheme'
+    'instrumentalforme_initializeTheme'
 );
 
 
-if (!function_exists('grayscale_initializeTheme')) {
-    function grayscale_initializeTheme()
+if (!function_exists('instrumentalforme_initializeTheme')) {
+    function instrumentalforme_initializeTheme()
     {
         add_theme_support('title-tag');
         add_theme_support('post-thumbnails');
@@ -24,22 +24,22 @@ if (!function_exists('grayscale_initializeTheme')) {
     }
 }
 
-if (!function_exists('grayscale_loadAssets')) {
-    function grayscale_loadAssets()
+if (!function_exists('instrumentalforme_loadAssets')) {
+    function instrumentalforme_loadAssets()
     {
         wp_enqueue_style(
-            'grayscale-styles',
+            'instrumentalforme-styles',
             get_theme_file_uri('css/styles.css')
         );
 
         wp_enqueue_style(
             'google-font',
-            'https://fonts.googleapis.com/css?family=Varela+Round',
-            'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i'
+            'https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900',
+            'https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i'
         );
 
         wp_enqueue_script(
-            'grayscale-scripts',
+            'instrumentalforme-scripts',
             get_theme_file_uri('js/scripts.js'),
             [],
             '1.0.0',
@@ -49,7 +49,6 @@ if (!function_exists('grayscale_loadAssets')) {
         wp_enqueue_script(
             'https://use.fontawesome.com/releases/v5.15.4/js/all.js',
             'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js',
-            'https://cdn.startbootstrap.com/sb-forms-latest.js'
         );
     }
 }
--- public/content/themes/instrumentalforme/index.php
+++ public/content/themes/instrumentalforme/index.php
@@ -95,10 +95,6 @@
             <p class="m-0 text-center text-white small">Copyright &copy; Your Website 2021</p>
         </div>
     </footer>
-    <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Core theme JS-->
-    <script src="js/scripts.js"></script>
     <?php get_footer(); ?>
 </body>
 
