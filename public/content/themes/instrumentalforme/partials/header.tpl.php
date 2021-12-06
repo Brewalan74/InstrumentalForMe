2a73fc1ec127ebad8e5160d786bd0c4a945e17fa
Brewalan74 <skeravelorigin@gmail.com>
2021-12-06 13:54:56 +0100

    Style update header

--- public/content/themes/instrumentalforme/partials/header.tpl.php
+++ public/content/themes/instrumentalforme/partials/header.tpl.php
@@ -3,7 +3,7 @@
         <div class="container px-5">
             <h1 class="masthead-heading mb-0">Instrumental For Me!</h1>
             <h2 class="masthead-subheading mb-0">Apprenez à jouer d'un instrument</h2>
-            <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Nos instruments</a>
+            <!-- <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Nos instruments</a> -->
         </div>
     </div>
     <div class="bg-circle-1 bg-circle"></div>
