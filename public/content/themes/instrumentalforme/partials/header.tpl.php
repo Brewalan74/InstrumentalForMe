57e7b484c5d3996bda8aa6a7dfc84300bd7f6a04
Brewalan74 <skeravelorigin@gmail.com>
2021-12-06 10:49:19 +0100

    Style add social buttons, add singular.php

--- public/content/themes/instrumentalforme/partials/header.tpl.php
+++ public/content/themes/instrumentalforme/partials/header.tpl.php
@@ -1,9 +1,9 @@
 <header class="masthead text-center text-white">
     <div class="masthead-content">
         <div class="container px-5">
-            <h1 class="masthead-heading mb-0">One Page Wonder</h1>
-            <h2 class="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
-            <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Learn More</a>
+            <h1 class="masthead-heading mb-0">Instrumental For Me!</h1>
+            <h2 class="masthead-subheading mb-0">Apprenez à jouer d'un instrument</h2>
+            <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Nos instruments</a>
         </div>
     </div>
     <div class="bg-circle-1 bg-circle"></div>
