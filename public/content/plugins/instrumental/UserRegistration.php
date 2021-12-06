75e2f611c6e7133eff296791f5ec132477f369f2
Stéphanie-Bornarel <emailutilisersur@github.comstefbornarel@gmail.com>
2021-12-06 15:31:51 +0100

    push for issue

--- public/content/plugins/instrumental/UserRegistration.php
+++ public/content/plugins/instrumental/UserRegistration.php
@@ -8,10 +8,11 @@ class UserRegistration
 {
     public function __construct()
     {
-        add_action( //Charger un css custom sur nos pages login & register
-            'login_enqueue_scripts',
-            [$this, 'loadAssets']
-        );
+      
+        // add_action( //Charger un css custom sur nos pages login & register
+        //     'login_enqueue_scripts',
+        //     [$this, 'loadAssets']
+        // );
 
         add_action( // Inserrer du code Html dans le formulaire de WP afin de le personnaliser
             'register_form',
@@ -90,7 +91,119 @@ class UserRegistration
     public function setUserPassword($newUserId)
     {
         $password = filter_input(INPUT_POST,'user_password');
+        wp_set_password($password, $newUserId);
     }
 
+    /*===============================
+           Cotrôle du formulaire
+      =============================== */
 
+    public function chekErrors($errors)
+    {
+        $password0 = filter_input(INPUT_POST, 'user_password');
+        $password1 = filter_input(INPUT_POST, 'user_password_confirmation');
+        $role = filter_input(INPUT_POST,'user_type');
+        $allowedRoles = [
+            'teacher',
+            'student'
+        ];
+        if(!in_array($role,$allowedRoles)) {
+            $errors->add(
+                'role-different',
+                '<strong>' . __('error: ') .'</strong> Rôle invalide'
+            );
+        }
+        if($password0 !== $password1) {
+            $errors->add(
+                'passwords-different',
+                '<strong>' . __('error: ') .'</strong> Le deuxième mot de passe doit correspondre au premier'
+            );
+        }
+        if(mb_strlen($password0) < 8) {
+            $errors->add(
+                'password-too-short',
+                '<strong>' . __('Error: ') . '</strong> Votre mot de passe doit contenir huit caractères '
+            );
+        }
+        if(!preg_match('/[A-Z]/', $password0)) {
+            $errors->add(
+                'password-no-capitalized-letter',
+                '<strong>' . __('Error: ') . '</strong> Votre mot de passe doit contenir un lettre majuscule '
+            );
+        }
+        if(!preg_match('/[a-z]/', $password0)) {
+            $errors->add(
+                'password-no-lowercase-letter',
+                '<strong>' . __('Error: ') . '</strong> Votre mot de passe doit contenir un lettre minuscule '
+            );
+        }
+
+        if(!preg_match('/[0-9]/', $password0)) {
+            $errors->add(
+                'password-no-number',
+                '<strong>' . __('Error: ') . '</strong> Votre mot de passe doit contenir un chiffre '
+            );
+        }
+
+
+        if(!preg_match('/\W/', $password0)) {
+            $errors->add(
+                'password-no-special-character',
+                '<strong>' . __('Error: ') . '</strong> Votre mot de passe doit contenir un caractère special '
+            );
+        }
+        return $errors;
+    }
+
+    /*===============================
+        Customisation du formulaire
+      =============================== */
+
+
+    //   public function loadAssets()
+    //   {
+        
+    //       wp_enqueue_style(
+    //           'login-form-css',
+    //           get_theme_file_uri('assets/css/user-registration.css')
+    //       );
+    //   } 
+
+     public function addCustomFields() 
+     {
+        
+         echo '
+            <p>
+                <label for="user_password">Mot de passe</label>
+                <input type="text" name="user_password" id="user_password" class="input" value="" size="20" autocapitalize="off">
+            </p>
+
+            <p>
+                <label for="user_password_confirmation">Confirmer votre mot de passe</label>
+                <input type="text" name="user_password_confirmation" id="user_password_confirmation" class="input" value="" size="20" autocapitalize="off">
+            </p>
+
+            <p>
+                <label for="user_password_confirmation">Je m\'inscrit en tant que </label>
+                <select id="user_type" name="user_type">
+                    <option value="teacher">Professeur</option>
+                    <option value="student">Elève</option>
+            </select>
+            </p>';
+
+                if('teacher' === 'true' ){
+                   echo'
+                    <p>
+                        <label for="certificate">Je selectionne mon diplôme</label>
+                        <select id="certificate" name="certificate">
+                            <option value="bts">bts</option>
+                            <option value="prepa">prepa</option>
+                        </select>
+                    </p>';
+                }
+                
+ 
+
+         
+     }
 }
