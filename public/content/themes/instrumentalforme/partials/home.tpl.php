7e6be854d23752da5cc1acfc3e5492c245d2c8fc
StephanieMenard <nena.stf7@gmail.com>
2021-12-07 08:27:10 +0100

    Add picture instrument for home page in folder img

--- public/content/themes/instrumentalforme/partials/home.tpl.php
+++ public/content/themes/instrumentalforme/partials/home.tpl.php
@@ -9,15 +9,24 @@
             </div>
             <div class="col-lg-6 order-lg-1">
                 <div class="p-5">
-                    <h2 class="display-4">For those about to rock...</h2>
+                    <h2 class="display-4">
+                        
+
+
+
+
+                    </h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
                 </div>
             </div>
         </div>
     </div>
 </section>
+
+
+
 <!-- Content section 2-->
-<section>
+<!--<section>
     <div class="container px-5">
         <div class="row gx-5 align-items-center">
             <div class="col-lg-6">
@@ -31,9 +40,10 @@
             </div>
         </div>
     </div>
-</section>
+</section>-->
+
 <!-- Content section 3-->
-<section>
+<!--<section>
     <div class="container px-5">
         <div class="row gx-5 align-items-center">
             <div class="col-lg-6 order-lg-2">
@@ -47,4 +57,4 @@
             </div>
         </div>
     </div>
-</section>
+</section>-->
