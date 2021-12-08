7153b054f2aab0e47116cf10bd9d479894dec73a
Brewalan74 <skeravelorigin@gmail.com>
2021-12-08 16:02:47 +0100

    Style update tab layout of some pages

--- public/content/themes/instrumentalforme/archive.php
+++ public/content/themes/instrumentalforme/archive.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>One Page Wonder - Start Bootstrap Template</title>
+    <title>Instrumental For Me!</title>
     <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
 
 </head>
@@ -26,35 +26,35 @@
     <!-- Header-->
     <?php get_template_part('partials/header.tpl'); ?>
 
-<?php
-$term = get_queried_object();
-$termId = $term->term_id;
-$taxonomyImage = get_field('picture', 'instrument_' . $termId);
-//dump($term);
-//dump($taxonomyImage['url']);
-//exit;
-?>
-
-       
+    <?php
+    $term = get_queried_object();
+    $termId = $term->term_id;
+    $taxonomyImage = get_field('picture', 'instrument_' . $termId);
+    //dump($term);
+    //dump($taxonomyImage['url']);
+    //exit;
+    ?>
 
 
-<!-- Content section 1-->
-<section id="scroll">
-    <div class="container px-5">
-        <div class="row gx-5 align-items-center">
-            <div class="col-lg-6 order-lg-2">
-                <div class="p-5"><a href="#"><img class="img-fluid rounded-circle" src="<?= $taxonomyImage['url']; ?>" alt="..." /></a></div>
-            </div>
-            <div class="col-lg-6 order-lg-1">
-                <div class="p-5">
-                    <h2 class="display-4"><?= $term->name; ?></h2>
 
-                    <p><?= $term->description; ?></p>
+
+    <!-- Content section 1-->
+    <section id="scroll">
+        <div class="container px-5">
+            <div class="row gx-5 align-items-center">
+                <div class="col-lg-6 order-lg-2">
+                    <div class="p-5"><a href="#"><img class="img-fluid rounded-circle" src="<?= $taxonomyImage['url']; ?>" alt="..." /></a></div>
+                </div>
+                <div class="col-lg-6 order-lg-1">
+                    <div class="p-5">
+                        <h2 class="display-4"><?= $term->name; ?></h2>
+
+                        <p><?= $term->description; ?></p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 
 
 
@@ -68,4 +68,3 @@ $taxonomyImage = get_field('picture', 'instrument_' . $termId);
 </body>
 
 </html>
-
