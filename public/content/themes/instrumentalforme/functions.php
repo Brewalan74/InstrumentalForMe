d3817e45f7dd6868c8eecb3c162182202f0c81b7
Brewalan74 <skeravelorigin@gmail.com>
2021-12-09 15:15:25 +0100

    Refactor add first name and last name

--- public/content/themes/instrumentalforme/functions.php
+++ public/content/themes/instrumentalforme/functions.php
@@ -74,6 +74,51 @@ add_action(
     'instrumentalforme_loadAssets'
 );
 
+add_action('register_form', 'myplugin_register_form');
+function myplugin_register_form()
+{
+
+    $first_name = (!empty($_POST['first_name'])) ? trim($_POST['first_name']) : '';
+    $last_name = (!empty($_POST['last_name'])) ? trim($_POST['last_name']) : '';
+
+?>
+    <p>
+        <label for="first_name"><?php _e('Prénom', 'mydomain') ?><br />
+            <input type="text" name="first_name" id="first_name" class="input" value="<?php echo esc_attr(wp_unslash($first_name)); ?>" size="25" /></label>
+    </p>
+
+    <p>
+        <label for="last_name"><?php _e('Nom', 'mydomain') ?><br />
+            <input type="text" name="last_name" id="last_name" class="input" value="<?php echo esc_attr(wp_unslash($last_name)); ?>" size="25" /></label>
+    </p>
+
+<?php
+}
+
+//2. Add validation. In this case, we make sure first_name and last_name is required.
+add_filter('registration_errors', 'myplugin_registration_errors', 10, 3);
+function myplugin_registration_errors($errors, $sanitized_user_login, $user_email)
+{
+
+    if (empty($_POST['first_name']) || !empty($_POST['first_name']) && trim($_POST['first_name']) == '') {
+        $errors->add('first_name_error', __('<strong>ERROR</strong>: Vous devez ajouter votre prénom.', 'mydomain'));
+    }
+    if (empty($_POST['last_name']) || !empty($_POST['last_name']) && trim($_POST['last_name']) == '') {
+        $errors->add('last_name_error', __('<strong>ERROR</strong>: Vous devez ajouter votre nom.', 'mydomain'));
+    }
+    return $errors;
+}
+
+//3. Finally, save our extra registration user meta.
+add_action('user_register', 'myplugin_user_register');
+function myplugin_user_register($user_id)
+{
+    if (!empty($_POST['first_name'])) {
+        update_user_meta($user_id, 'first_name', trim($_POST['first_name']));
+        update_user_meta($user_id, 'last_name', trim($_POST['last_name']));
+    }
+}
+
 
 add_filter('get_the_excerpt', function ($excerpt) {
 
