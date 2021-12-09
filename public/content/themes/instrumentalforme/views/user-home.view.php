5453f5cb2a07cd6ce9a44ae507dfffcdd1ca38e0
StephanieMenard <nena.stf7@gmail.com>
2021-12-09 16:08:35 +0100

    Feat update profile.view

--- public/content/themes/instrumentalforme/views/user-home.view.php
+++ public/content/themes/instrumentalforme/views/user-home.view.php
@@ -1,83 +1,189 @@
 <?php
 the_post();
 ?>
+<?php
+//$test = get_terms();
+//dump($test);  
+//RENVOIE TAXO ASSOCIEES AU PROFIL CONNECTE
+
+//$test = the_post();
+// dump($test);    
+//RENVOIE NULL 
+
+//$test = get_field('description');
+//dump($test);
+// RENVOIE NULL
+
+//$test = get_fields();
+//dump($test);
+//RENVOIE NULL
+
+//$test = get_the_post_type_description();
+//dump($test);
+// RENVOIE ""
+
+//$test = get_post_types();
+//dump($test); 
+//RENVOIE TABLEAU
+
+//$args = array ('name' => 'profile-teacher');
+//$test = get_post_types($args);
+//dump($test); 
+//RENVOIE TABLEAU "profile-teacher" => "profile-teacher"
+
+//$test = get_role('teacher');
+//dump($test);
+// RENVOIE TABLEAU CAPABILITIES
+
+//$test = have_posts();
+//dump($test);
+// RENVOIE TRUE
+
+//$test = get_post_meta(get_the_ID(), 'teacher', true);
+//dump($test);
+// RENVOIE ""
+
+$test = get_the_content();
+dump($test);
+?>
 <!DOCTYPE html>
-<!-- STEP E7 THEME déclaration de la langue utilisée sur le site //-->
-<html lang="<?=get_bloginfo('language');?>">
+<html lang="<?= get_bloginfo('language'); ?>">
 
 <head>
-    <?php
-    get_header();
-    ?>
+    <!-- wp header -->
+    <?php get_header(); ?>
 </head>
 
 <body>
-    <!--================================================================================-->
 
-    <main>
-        <?php
-            get_template_part('partials/navbar.tpl');
-        ?>
-        <!--================================================================================-->
+    <!-- Navigation-->
+    <?php get_template_part('partials/navbar.tpl'); ?>
+
+    <!-- Header-->
+    <?php get_template_part('partials/header.tpl'); ?>
 
 
-        <?php
-        /*$backgroundImage = get_theme_mod('hero-picture');
+    <?php 
+    $user = wp_get_current_user();
+    $roles = $user->roles;
 
-        $style = '';
-        if($backgroundImage) {
-            $style = 'background:' .
-                'radial-gradient(circle, rgba(255, 255, 255, 0.5) 45%, rgba(136, 81, 133, 0.5) 100%),' .
-                'linear-gradient(100deg, aqua 0%, rgba(0, 255, 255, 0) 100%),'.
-                'url(' . $backgroundImage . ')'
-            ;
-        }*/
-        ?>
+    if(in_array('teacher', $roles)) {
+        $isTeacher = true;
+    }
+    else {
+        $isTeacher = false;
+    }
 
-        <section class="hero">
-            <h1 class="hero__title">
-                <?php
+    if ($isTeacher) {
+        echo '<section class="name-profil-perso text-center">
+            <h1 class="profil-perso"> ';
+                
                     if(is_user_logged_in()) {
                         $user = wp_get_current_user();
                         echo "Bonjour " . $user->display_name;
                         //dump($user);
-                    }
-                ?>
-            </h1>
-        </section>
+                    };
+            '</h1>';           
+        echo '</section>
+        
+        <p class="text-center">'; 
+        echo get_avatar(
+            $post->ID,
+            $size = 96,
+            $default = '',
+            $alt = '',
+            $args = null
+        );'</p>
 
-        <section>
-            <p><?= get_the_content(); ?></p>
-        </section>
+        <div>
+        <p class="text-end mx-5"><a class="fs-5 text-end link-profil" href="#">Modifier votre profile</a></p>
+        </div>
 
 
-        <section>
+        <section class="m-5 text-center description-perso">
+            <h2 class="border">'; echo get_the_content(); '</h2>
+        </section>';
+
+
+        echo '<section class="m-5">
+            <div class="container container-recap">
+            
+            <ul class="recap m-5">
             <h3>Vos nouvelles demandes de RDV</h3>
-        </section>
-
-
-        <section>
+                <li>Eleve / Date + Heure A DYNAMISER</li>
+                <button type="button" class="btn btn-success">Valider</button>
+                <button type="button" class="btn btn-danger">Refuser</button>
+            </ul>
+            
+            <ul class="recap m-5">
             <h3>Liste de vos cours</h3>
-        </section>
-
-
-        <section>
+                <li><a class="link-profil"href="#">Cours 1 A DYNAMISER</a></li>
+            </ul>
+            
+            <ul class="recap m-5">
             <h3>Liste de vos élèves</h3>
-        </section>
+                <li><a class="link-profil" href="#">Eleve 1 A DYNAMISER</a></li>
+            </ul>
+        </section>';
+    } else {
+        echo '<section class="name-profil-perso">
+            <h1 class="profil-perso text-center"> ';
+                
+                    if(is_user_logged_in()) {
+                        $user = wp_get_current_user();
+                        echo "Bonjour " . $user->display_name;
+                        dump($user);
+                    };
+            '</h1>';
+            
+        echo'</section>
+
+        <p class="text-center">'; 
+        echo get_avatar(
+            $post->ID,
+            $size = 96,
+            $default = '',
+            $alt = '',
+            $args = null
+        );'</p>
+
+        <div>
+        <p class="text-end mx-5"><a class="fs-5 text-end link-profil" href="#">Modifier votre profile</a></p>
+        </div>
 
 
-    </main>
+        <section class="m-5 text-center description-perso">
+            <p class="border border-primary" style="border: 2px solid red;";>'; echo get_the_content(); '</p>
+        </section>';
 
-    <footer class="section footer">
-        <?php
-            get_template_part('partials/footer.tpl');
-            /*get_template_part('partials/footer-menu.tpl');
-            get_template_part('partials/footer-social-links.tpl');  */
-        ?>
-    </footer>
 
-    <?php
-    get_footer();
+        echo '<section class="m-5">
+            <div class="container container-recap">
+            
+            <ul class="recap m-5">
+            <h3>Vos prochains cours</h3>
+                <li>Date - Heure - Prof A DYNAMISER</li>
+                <button type="button" class="btn btn-danger">Annuler le RDV</button>
+            </ul>
+            
+            <ul class="recap m-5">
+            <h3>Vos professeurs</h3>
+                <li><a class="link-profil" href="#">Prof / instrument A DYNAMISER</a></li>
+            </ul>
+            
+        </section>';
+    }
     ?>
+
+      
+<div>
+
+    <!-- Footer-->
+    <?php get_template_part('partials/footer.tpl'); ?>
+</div>
+    <!-- wp footer -->
+    <?php get_footer(); ?>
 </body>
+
 </html>
+
