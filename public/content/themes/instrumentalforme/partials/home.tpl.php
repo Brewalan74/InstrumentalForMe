41753749be9dd137f139bbd9daf3d0d9e67c3505
Brewalan74 <skeravelorigin@gmail.com>
2021-12-09 10:09:30 +0100

    Style update layout home and navbar

--- public/content/themes/instrumentalforme/partials/home.tpl.php
+++ public/content/themes/instrumentalforme/partials/home.tpl.php
@@ -1,49 +1,49 @@
     <?php
-$terms = get_terms( array(
-    'taxonomy' => 'instrument',
-    'hide_empty' => false,
-) );
-?>
+    $terms = get_terms(array(
+        'taxonomy' => 'instrument',
+        'hide_empty' => false,
+    ));
+    ?>
 
 
 
 
-<!-- section need dynamization -->
+    <!-- section need dynamization -->
 
-<!-- Content section 1-->
-<?php 
-for($i = 0; $i < count($terms); $i++):
-    $taxonomyImageData = get_field('picture', 'instrument_' . $terms[$i]->term_id);
-    $taxonomyImage = $taxonomyImageData['url'];
-    //dump($taxonomyImageData);
-    //dump($terms[$i]);
-    // $taxonomyImage = $terms[$i]->term_id;
-     //dump($taxonomyImage);
-?>
-<section class="instrument-container">
-    <div class="container px-5">
-        <div class="row gx-5 align-items-center instrument">
-            <div class="col-lg-6 order-lg-2">
-                <!--
+    <!-- Content section 1-->
+    <?php
+    for ($i = 0; $i < count($terms); $i++) :
+        $taxonomyImageData = get_field('picture', 'instrument_' . $terms[$i]->term_id);
+        $taxonomyImage = $taxonomyImageData['url'];
+        //dump($taxonomyImageData);
+        //dump($terms[$i]);
+        // $taxonomyImage = $terms[$i]->term_id;
+        //dump($taxonomyImage);
+    ?>
+        <section class="instrument-container">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center instrument">
+                    <div class="col-lg-6 order-lg-2">
+                        <!--
                 <div class="p-5"><a href="#"><img class="img-fluid rounded-circle" src="<?= $taxonomyImage; ?>" alt="..." /></a></div>
                 //-->
-                <div class="p-5 instrument__picture" style="background-image: url(<?= $taxonomyImage; ?>)"></div>
-            </div>
-            <div class="col-lg-6 order-lg-1 instrument__description">
-                <div class="p-5">
-                    <h2 class="display-4"><a href="<?=get_term_link($terms[$i]->term_id);?>"><?= $terms[$i]->name; ?></a></h2>
+                        <div class="p-5 instrument__picture" style="background-image: url(<?= $taxonomyImage; ?>)"></div>
+                    </div>
+                    <div class="col-lg-6 order-lg-1 instrument__description">
+                        <div class="p-5">
+                            <h2 class="display-4"><a href="<?= get_term_link($terms[$i]->term_id); ?>"><?= $terms[$i]->name; ?></a></h2>
 
-                    <p><?= get_the_excerpt(); ?></p>
+                            <p><?= substr($terms[$i]->description, 0, 500) . '...'; ?></p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</section>
-<?php endfor; ?>
+        </section>
+    <?php endfor; ?>
 
 
-<!-- Content section 2-->
-<!--<section>
+    <!-- Content section 2-->
+    <!--<section>
     <div class="container px-5">
         <div class="row gx-5 align-items-center">
             <div class="col-lg-6">
@@ -59,8 +59,8 @@ for($i = 0; $i < count($terms); $i++):
     </div>
 </section>-->
 
-<!-- Content section 3-->
-<!--<section>
+    <!-- Content section 3-->
+    <!--<section>
     <div class="container px-5">
         <div class="row gx-5 align-items-center">
             <div class="col-lg-6 order-lg-2">
