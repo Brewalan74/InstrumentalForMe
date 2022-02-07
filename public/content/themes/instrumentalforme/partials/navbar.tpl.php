79be8779479deabbd4b14d95a45d2b2e39a5e62a
Brewalan <sbpk.pro@gmail.com>
2022-02-07 14:57:29 +0100

    Review clean code in theme partials folder

--- public/content/themes/instrumentalforme/partials/navbar.tpl.php
+++ public/content/themes/instrumentalforme/partials/navbar.tpl.php
@@ -4,27 +4,23 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
-                <!-- <li class="nav-item"><a class="nav-link" href="#!">Inscription</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">Connexion</a></li> -->
 
                 <?php
-                if (!is_user_logged_in()) {
-                    echo '<li class="navbarLayout-li"><a href="' . wp_login_url() . '">Se connecter</a></li>';
-                    echo '<li class="navbarLayout-li"><a href="' . wp_registration_url() . '">S\'inscrire</a></li>';
-                } else {
+                if (!is_user_logged_in()) : ?>
+                    <li class="navbarLayout-li"><a href="<?= wp_login_url() ?>">Se connecter</a></li>
+                    <li class="navbarLayout-li"><a href="<?= wp_registration_url() ?>">S\'inscrire</a></li>
+                <?php else :
                     $user = wp_get_current_user();
                     global $router;
-                    // dump($user);
                     if (in_array('teacher', $user->roles)) {
                         $url = $router->generate('user-home');
                     } else {
                         $url = $router->generate('user-home');
                     }
-
-                    echo '<li class="navbarLayout-li"><a href="' . $url . '">' . 'Mon profil' . '</a></li>';
-                    echo '<li class="navbarLayout-li"><a href="' . wp_logout_url() . '">Se déconnecter</a></li>';
-                }
                 ?>
+                    <li class="navbarLayout-li"><a href="<?= $url ?>">Mon profil</a></li>
+                    <li class="navbarLayout-li"><a href="<?= wp_logout_url() ?>">Se déconnecter</a></li>
+                <?php endif; ?>
 
             </ul>
         </div>
